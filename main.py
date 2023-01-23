5bec1b5b4a5f14734380712e7f64dbd64be95bc7
RaeTennent <110914424+RaeTennent@users.noreply.github.com>
2023-01-23 08:32:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-# Default template for Digi projects
+import time
+from machine import I2C, Pin
+from Lib import SCD30
 
-print("Hello World!")
+i2c = I2C(1)
+scd30 = SCD30.SCD30(i2c, 97)
+
+while True:
+    # Wait for sensor data to be ready to read (by default every 2 seconds)
+    while scd30.get_status_ready() != 1:
+        time.sleep_ms(200)
+    scd30.read_measurement()
