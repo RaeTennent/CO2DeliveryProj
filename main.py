1b56da80fb79dbcb4434d04f66f89fcc4e705ae6
RaeTennent <110914424+RaeTennent@users.noreply.github.com>
2023-01-23 10:12:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import time
 from machine import I2C, Pin
 from Lib import SCD30
+from Lib import BME280
 
 i2c = I2C(1)
+i2c1 = I2C(1, freq=400000)
 scd30 = SCD30.SCD30(i2c, 97)
+bme = BME280.BME280(i2c=i2c1)
+#TARGET_64BIT_ADDR = b'\x00\x00\x00\x00\x00\x00\x00\x00'
+MESSAGE = ""
+#sleep_ms = 0
+sampleRate = 60000 #milli-seconds
+separator = " | "
 
 while True:
+    temp = bme.temperature
+    hum = bme.humidity
+    presh = bme.pressure
+
     # Wait for sensor data to be ready to read (by default every 2 seconds)
     while scd30.get_status_ready() != 1:
         time.sleep_ms(200)
-    scd30.read_measurement()
+    #m = scd30.read_measurement()
+    #print(m)
+    co2 = scd30.CO2
+    MESSAGE = str(temp) + separator + str(hum) + separator + str(presh) + separator + co2
+    print(MESSAGE)
